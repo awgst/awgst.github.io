af3c76bf72b05e241cde1dbcd12a12baaa41ede6
awgst <trisaktiawang@gmail.com>
2022-01-23 15:54:16 +0700

    test lagi

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,8 @@ import { Game } from "./Game.js";
 const canvas = document.querySelector('canvas');
 const content = canvas.getContext('2d');
 
+content.canvas.style.background = '#fff';
+
 const game = new Game();
 
 game.start();
